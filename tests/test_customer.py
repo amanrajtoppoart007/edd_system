cd1b19a881c7e0fcf29ebb58b6e4d4f3943c5143
Aman Raj <42941876+amanrajtoppoart007@users.noreply.github.com>
2025-05-13 17:09:15 +0530

    updated files

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -3,9 +3,9 @@ import unittest
 from models.customer import Customer
 from utils.utils import Utils
 
-class TestJob(unittest.TestCase):
+class TestCustomer(unittest.TestCase):
     def test_customer_creation(self):
-        customer = Customer("Test Customer","Test parts","Test Location")
+        customer = Customer("Test Customer",Utils.generate_random_email())
         customer.save()
         customers = Customer.get_all()
         self.assertTrue(any(c[1] == "Test Customer" for c in customers))
